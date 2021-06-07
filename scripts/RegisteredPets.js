33d489503a6df81db16e9687405d14d14f678996
lcurtis4 <lcurtis0104@gmail.com>
2021-06-07 14:16:22 -0500

    pet pop up

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,4 +1,26 @@
 import { getPets } from "./database.js"
+import { getWalkers } from "./database.js"
+
+const walkers = getWalkers()
+
+document.addEventListener( 
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target 
+        if (itemClicked.id.startsWith("pet")) {
+            const [,petId] = itemClicked.id.split("--") 
+            
+            for (const pet of pets) {
+                if (pet.id === parseInt(petId)) {
+                    let walkedPet = pets.find(pet => pet.id === parseInt(petId))
+                    let walker = walkers.find(walker => walker.id === walkedPet.walkerId)
+
+                    window.alert(`${pet.name} is being walked by ${walker.name}.`)
+                }
+            }
+        }
+    }
+)
 
 const pets = getPets()
 
@@ -6,7 +28,7 @@ export const RegisteredPets = () => {
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li id="pet--${pet.id}">${pet.name}</li>`
     }
 
     petHTML += "</ul>"
